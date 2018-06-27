992d6ecf077baeae6f726c8984592f04b6b51d4c
Mordechai Meisels <mordechai112@gmail.com>
2018-06-27 14:16:02 -0400

    Updated mojo

--- src/main/java/org/update4j/maven/Update4jMojo.java
+++ src/main/java/org/update4j/maven/Update4jMojo.java
@@ -4,12 +4,26 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -20,6 +34,8 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.update4j.Configuration;
 import org.update4j.Library;
 import org.update4j.binding.ConfigBinding;
+import org.update4j.binding.LibraryBinding;
+import org.update4j.util.FileUtils;
 
 @Mojo(name = "sync")
 public class Update4jMojo extends AbstractMojo {
@@ -71,14 +87,93 @@ public class Update4jMojo extends AbstractMojo {
 			base = basePath.toPath();
 		}
 
-		for (Library lib : config.getLibraries()) {
-			Path p = base.resolve(config.getBasePath().relativize(lib.getPath()));
+		PrivateKey key = null;
+		if (keystore != null) {
+			Properties prop = new Properties();
+			try (InputStream in = Files.newInputStream(keystore.toPath())) {
+				prop.load(in);
 
-			if (removeUnmatched && Files.notExists(p)) {
-				configBinding.libraries
-								.removeIf(libBinding -> Long.parseLong(libBinding.checksum, 16) == lib.getChecksum());
+				String location = getProperty(prop, "keystore");
+				char[] password = prop.getProperty("keystorePassword", "").toCharArray();
+				String alias = getProperty(prop, "alias");
+				char[] aliasPw = prop.getProperty("aliasPassword", "").toCharArray();
+
+				try {
+					File ksFile = new File(location);
+					KeyStore ks = KeyStore.getInstance(ksFile, password);
+
+					key = (PrivateKey) ks.getKey(alias, aliasPw);
+				} catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | UnrecoverableKeyException
+								| ClassCastException e) {
+					throw new MojoFailureException("Failed to read keystore: " + location, e);
+				}
+
+			} catch (IOException e) {
+				throw new MojoFailureException("Failed to load keystore properties files: " + keystore);
 			}
 		}
+
+		boolean changed = false;
+		List<Integer> removes = new ArrayList<>();
+
+		for (int i = 0; i < config.getLibraries().size(); i++) {
+			Library lib = config.getLibraries().get(i);
+			LibraryBinding lb = configBinding.libraries.get(i);
+
+			if (lib.getPath() == null) {
+				getLog().info("Ignored foreign library: " + lb.path);
+				continue;
+			}
+
+			Path p = base.resolve(config.getBasePath().relativize(lib.getPath()));
+
+			if (Files.notExists(p)) {
+				if (removeUnmatched)
+					removes.add(i);
+				else
+					getLog().warn("Could not find library: " + lib.getPath());
+			}
+
+			try {
+				if (lib.requiresUpdate()) {
+					changed = true;
+
+					lb.size = Files.size(p);
+					lb.checksum = Long.toHexString(FileUtils.getChecksum(p));
+
+					if (key != null)
+						lb.signature = Base64.getEncoder().encodeToString(FileUtils.sign(p, key));
+					else
+						lb.signature = null;
+				}
+			} catch (IOException e) {
+				throw new MojoFailureException("Error reading library: " + p, e);
+			}
+
+		}
+
+		for (int remove : removes) {
+			configBinding.libraries.remove(remove);
+		}
+
+		if (changed) {
+			configBinding.timestamp = Instant.now().toString();
+		}
+
+		try (Writer out = Files.newBufferedWriter(configuration.toPath())) {
+			configBinding.write(out);
+		} catch (IOException e) {
+			throw new MojoFailureException("Could not write configuration file: " + configuration, e);
+		}
+	}
+
+	private String getProperty(Properties prop, String key) throws MojoFailureException {
+
+		String value = prop.getProperty(key);
+		if (value == null)
+			throw new MojoFailureException("Missing '" + key + "' property in '" + keystore + "'");
+
+		return value;
 	}
 
 }
